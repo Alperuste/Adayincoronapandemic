499a9d4ec509cb038246ab62e68c40cf89df2356
Alper Üste <alperuste@gmail.com>
2020-10-26 10:07:12 +0200

    before 26102020

---
+++ Adayincoronagame/Assets/Scripts/GameplayManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayManager : MonoBehaviour
+{
+    public int CoronaRisk;
+    public string textTitle;
+    public string textQuestion;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    #region DailyActivities
+        // Daily Activities
+
+    public void GoToHome()
+    {
+
+    }
+    public void GoToUni()
+    {
+
+    }
+    public void GoToCafe()
+    {
+
+    }
+    #endregion
+
+    #region TransportationDecisions
+        // Transportation Decisions
+
+    public void Walk()
+    {
+
+    }
+
+    public void PublicTransport()
+    {
+
+    }
+
+    public void Taxi()
+    {
+
+    }
+
+    #endregion
+
+    #region HealthDecisions
+    // Health Decisions
+
+    public void WearMask()
+    {
+
+    }
+
+    public void UseSanitizer()
+    {
+
+    }
+
+    public void WashYourHands()
+    {
+
+    }
+    #endregion
+
+    // Day/Night Shift
+
+
+}
