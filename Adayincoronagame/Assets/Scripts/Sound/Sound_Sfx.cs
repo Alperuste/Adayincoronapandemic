1bee679512e9682503793aadbd7016ddd5a55af5
Alawode 'Fiyikewa <55203377+Itsjustkewa@users.noreply.github.com>
2020-11-18 15:40:15 +0200

    Added Sound
    Added the Sound Manager
    Added SFX and BGM scripts
    Added Place Holder Sounds

---
+++ Adayincoronagame/Assets/Scripts/Sound/Sound_Sfx.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace CoronaGame.Audio
+{ 
+    public class Sound_Sfx : MonoBehaviour
+    {
+        public Sound SFX;
+
+        private void Start()
+        {
+        
+        }
+
+        public void PlaySFX()
+        {
+            SoundManager.instance.PlaySFX(SFX);
+        }
+    }
+}
