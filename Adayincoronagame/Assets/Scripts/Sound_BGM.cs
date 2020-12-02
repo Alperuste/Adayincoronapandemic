aef6420f7d3b9935be633fc549294490ab4548a3
Alper Üste <alperuste@gmail.com>
2020-12-02 15:11:31 +0200

    Screen Scalers added, Sounds added, Sound effects added

---
+++ Adayincoronagame/Assets/Scripts/Sound_BGM.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace CoronaGame.Audio
+{
+    public class Sound_BGM : MonoBehaviour
+    {
+
+        public Sound BGM;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            PlayBGM();
+            Debug.Log("bgm is playing");
+        }
+
+        public void PlayBGM()
+        {
+            SoundManager.instance.PlayBGM(BGM);
+        }
+    }
+}
